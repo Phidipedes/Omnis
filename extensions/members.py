09d4898c8af47e8514459cbb71e05b851145d26d
Phidipedes <trevorkjess@gmail.com>
2021-04-15 18:15:37 -0600

    added leave message

--- extensions/members.py
+++ extensions/members.py
@@ -93,6 +93,10 @@ class members(commands.Cog, name = "Member Updates"):
                 await trialsCollection.update_one({"_id": "envision"}, {"$pull": {"trialMembers": {"username": cachedData["members"][uuid]["username"]}}})
                 await activityCollection.update_one({"_id": "envision"}, {"$pull": {"whitelist": {"username": cachedData["members"][uuid]["username"]}}})
 
+                memberLeaveEmbed = discord.Embed(title = f"Member Left", description = f"UUID: {uuid}\nUsername: {currentUsername}\nRank: {currentRank}", color = discord.Color.dark_red(), timestamp = datetime.datetime.utcnow())
+
+                await memberLogChannel.send(embed = memberLeaveEmbed)
+
     @updateMembers.before_loop
     async def beforeUpdateMembers(self):
 
